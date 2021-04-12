4c0ec4e4af101a1a25448dcbd6145e3d6fb4931b
a-taghva <taghvali99@gmail.com>
2021-04-12 01:58:06 -0400

    methods for finding pizzas

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,31 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+  // get all pizzas
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // get one pizza by id
+  getPizzaById({ parmas }, res) {
+    Pizza.findOne({ _id: parmas.id })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          return res.status(404).json({ message: 'No pizza found with this id!'});
+        }
+
+        res.json(dbPizzaData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+}
+
+module.exports = pizzaController;
