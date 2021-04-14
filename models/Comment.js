0f83178f79854f1bb1f3762557e4c2ca45ac5a21
a-taghva <taghvali99@gmail.com>
2021-04-14 04:32:16 -0400

    replySchema and association with comment

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,31 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid ocnfusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String
+    },
+    writtenBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+)
 
 const CommentSchema = new Schema(
   {
@@ -13,12 +39,15 @@ const CommentSchema = new Schema(
       type: Date,
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
-    }
+    },
+    replies: [ReplySchema]
   },
   {
     toJSON: {
-      getters: true
-    }
+      getters: true,
+      virtuals: true,
+    },
+    id: false
   }
 );
 
